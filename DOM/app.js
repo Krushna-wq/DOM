e839e9df79fc68eb8d82f1c88e96004b8927a875
Krushna <krish762090@gmail.com>
2026-02-11 18:38:08 +0530

    modified

--- DOM/app.js
+++ DOM/app.js
@@ -1,1 +1,22 @@
-console.log("starting dom");
+let btn = document.querySelector(".addbtn");
+let ul = document.querySelector("ul");
+let inp = document.querySelector("#ip");
+
+
+btn.addEventListener("click", function() {
+     let item = document.createElement("li"); 
+     item.innerText = inp.value;
+
+     let delbtn = document.createElement("button");
+     delbtn.innerText = "Delete";
+     ul.appendChild(item);
+    item.appendChild(delbtn);
+   inp.value = "";
+});
+
+ul.addEventListener("click", function (event) {
+   if(event.target.nodeName == "BUTTON"){
+    let itemDel = event.target.parentElement; 
+    itemDel.remove();
+   }
+});
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Learning JavaScript DOM basics.
+# js-dom-practice
+Practicing JavaScript DOM basics like selectors, events, and element changes.
+
+Topics:
+- Element selection
+- Event listeners
+- Text and style manipulation
+- Input handling
+- Creating elements
