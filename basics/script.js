4ee5a31a2c6991b548b56b8cb0598b032dc0e61c
Krushna <krish762090@gmail.com>
2026-02-12 08:02:14 +0530

    DOM mini task: background color change

--- basics/script.js
+++ basics/script.js
@@ -1,13 +1,28 @@
 let ipt = document.querySelector(".ip");
 let btn = document.querySelector(".press");
 // let head = document.querySelector("h1");
-let body = document.querySelector("body");
+// let body = document.querySelector("body");
 
 btn.addEventListener("click", function () {
 
  let head = document.createElement("h1");
  head.innerText = ipt.value;
- body.appendChild(head);
+//  body.appendChild(head);
   ipt.value = "";
+});
+
+
+let Cbtn = document.querySelector(".Colorbtn");
+let body = document.querySelector("body");
+
+Cbtn.addEventListener("click", function () {
+  let red = Math.floor(Math.random()*256);
+  let green = Math.floor(Math.random()*256);
+  let blue = Math.floor(Math.random()*256);
+
+  body.style.backgroundColor =  (`rgb(${red}, ${green}, ${blue})`);    // rgb(122, 102, 120);
+  
+
+  
 
 });
