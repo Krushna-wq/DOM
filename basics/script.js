bb62e1f4e27f336834ea0b6d12635b502fa2f4e7
Krushna <krish762090@gmail.com>
2026-02-13 18:00:18 +0530

    Toggler with display

--- basics/script.js
+++ basics/script.js
@@ -4,7 +4,7 @@ const item = document.querySelector("div");
 
 btn.addEventListener("click", function () {
  
-    item.classList.toggle("highlight");
+    item.classList.toggle("hidden");
    
 
 });
