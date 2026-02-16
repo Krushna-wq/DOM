c460a177d4101685cf09ea192c2bc6ce408a23cd
Krushna <krish762090@gmail.com>
2026-02-16 23:50:52 +0530

    DOM + object - login state handling

---
+++ dom/login-status-object/app.js
@@ -0,0 +1,37 @@
+const user = {
+    name: "benny",
+    Isloggedin: false,
+}
+
+const loginBtn = document.getElementById("login");
+const LogoutBtn = document.getElementById("logout");
+const text = document.querySelector("h2");
+const body = document.querySelector("body");
+const line = document.querySelector("p");
+
+loginBtn.addEventListener("click", function () {
+    
+    user.Isloggedin = true;
+
+ 
+    console.dir(user);
+    text.textContent = "User logged in";
+    line.textContent  = `Welcome ${user.name}`;
+
+    body.appendChild(line);
+ 
+})
+
+LogoutBtn.addEventListener("click", function () {
+
+    user.Isloggedin = false;
+
+     
+
+     console.dir(user);
+     line.textContent = `${user.name} your account has been logged Out`;
+     text.textContent = "User not Logged in";
+
+     body.appendChild(line);
+
+});
