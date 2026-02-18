90ea94c676e534d9b6a54e5ca6e8be703fca5576
Krushna <krish762090@gmail.com>
2026-02-18 16:19:35 +0530

    DAY: 7 used destructuring with dom

---
+++ dom/practice-projects/destructuring-user/app.js
@@ -0,0 +1,20 @@
+const user = {
+    username: "XYZ",
+    age: 20,
+    city:"pune"
+}
+
+let {username, age, city} = user;
+
+const btn =  document.querySelector(".clk");
+const para1 = document.querySelector(".text1");
+const para2 = document.querySelector(".text2");
+const para3 = document.querySelector(".text3");
+
+btn.addEventListener("click", function () {
+
+     para1.textContent =  `username: ${username}`;
+     para2.textContent = `age: ${age}`;
+     para3.textContent = `city: ${city}`;
+ 
+});
