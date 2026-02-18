ff0314c345f8ae8371bed2bd79251603a4a66bcb
Krushna <krish762090@gmail.com>
2026-02-18 15:46:17 +0530

    learning functions

---
+++ functions/basics/app.js
@@ -0,0 +1,43 @@
+// function addTwoNum (a, b) {
+//     console.log(a + b);
+// }
+
+// addTwoNum(2, 4);
+
+
+function LoginUserMsge (username){
+    return `${username} just logged in `;
+}
+
+// console.log(LoginUserMsge("sam"));
+
+function CartPrice(var1, var2, ...num1) {         // (...num) rest
+    return num1 
+}
+
+// console.log(CartPrice(200, 300, 400, 20000));
+
+const Product = {
+    username: "sam",
+    price: 200
+}
+
+function handleBy(anyobject) {    //(anyobject or object name)
+    // console.log(`username is ${anyobject.username} price is ${anyobject.price}`)
+}
+
+// handleBy(Product);
+
+// handleBy({
+//     username: "PK",    //direct passing with object 
+//     price: 899    
+// })
+
+
+const Array = [100, 300, 200, 600, 899];
+
+function returnSecondValue(getarray) {
+    return getarray[1]
+}
+
+console.log(returnSecondValue(Array));    //pass array
