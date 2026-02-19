1bb4825472b87257fbda5c617a2d7370ebff839c
Krushna <krish762090@gmail.com>
2026-02-19 20:24:02 +0530

    DAY 8: builder basic counter

---
+++ dom/practice-projects/counter/script.js
@@ -0,0 +1,19 @@
+let counter = 0;
+function increase(){
+  counter++;
+}
+
+const btn = document.querySelector("button");
+const body = document.querySelector("body");
+
+btn.addEventListener("click", function(){
+  increase();
+  const number = document.createElement("p");
+    number.textContent = counter;
+    body.appendChild(number);
+})
+  
+ 
+      
+
+  
