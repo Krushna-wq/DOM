760667383d6cca93183aa7cd6bdb325257f0ef06
Krushna <krish762090@gmail.com>
2026-02-22 01:08:30 +0530

    building table counter

---
+++ dom/practice-projects/table-counter/app.js
@@ -0,0 +1,19 @@
+const ipt = document.querySelector(".num");
+const btn = document.querySelector(".btn");
+const body = document.querySelector("body")
+
+function table(){   
+    const num = ipt;
+    for(let i = 1; i<=num; i++){
+      let Final = i * i;
+      console.log(final);
+    }
+}
+
+btn.addEventListener("click", function {
+       const newline = document.createElement("p");
+    
+    newline.textContent = table;
+
+    body.appendChild(newline);
+})
